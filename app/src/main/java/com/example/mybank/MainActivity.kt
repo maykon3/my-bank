46b9e461607ee927a2f932c322c9aea1c3bffe5f
maykon3 <mkn.silva29@gmail.com>
2025-04-10 01:39:01 -0300

    style: backgroud cartoes

--- app/src/main/java/com/example/mybank/MainActivity.kt
+++ app/src/main/java/com/example/mybank/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.example.mybank
 
 import android.os.Bundle
+import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mybank.ui.theme.MyBankTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,19 +33,32 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyBankTheme {
-                    Inicio()
-                }
+                Inicio()
             }
         }
     }
+}
 
 
 @Composable
 fun Inicio() {
-    Column (
-        Modifier.fillMaxSize()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
             .background(Color.White)
-    ){
+
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .paint(
+                    painterResource(id = R.drawable.bg_main_mobile),
+                    contentScale = ContentScale.Crop
+                    )
+
+        ) {
+
+        }
 
     }
 
